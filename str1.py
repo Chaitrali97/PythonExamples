a52ef01e981750827e3cfcf8d05651ebced4f648
Chaitrali97 <chaitralee71@gmail.com>
2018-12-10 14:55:45 +0530

    new file added

---
+++ str1.py
@@ -0,0 +1,11 @@
+message = "God bless you!!!"
+print(message)
+
+message2 = "God bless you my dear best friend!!!"
+print(message2)
+
+message3 = 'I\'m Chaitraliii.'
+print(message3)
+
+message4 = "I'm Chaitraliii."
+print(message4)
